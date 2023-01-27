0f738045749476c252918e1b71f518eacc763207
MartinKotas <kotasmartin1997@gmail.com>
2023-01-27 19:40:37 +0000

    basic navbar

---
+++ mewe/src/App.js
@@ -0,0 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import ColorSchemesExample from "./components/Navbar";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <ColorSchemesExample />
+    </div>
+  );
+}
+
+export default App;
---
+++ mewe/src/components/Navbar.jsx
@@ -0,0 +1,55 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+function ColorSchemesExample() {
+  return (
+    <>
+      <Navbar bg="dark" variant="dark" id="navbar">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              src="https://media.discordapp.net/attachments/1024594853008506920/1068560828032352377/MeWe_logo.jpg"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              alt="MeWe"
+            />
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link
+              id="navigation"
+              className="underline.gradient"
+              href="#navigation1"
+            >
+              Navigation1
+            </Nav.Link>
+            <Nav.Link
+              id="navigation"
+              className="underline.gradient"
+              href="#navigation2"
+            >
+              Navigation2
+            </Nav.Link>
+            <Nav.Link
+              id="navigation"
+              className="underline.gradient"
+              href="#navigation3"
+            >
+              Navigation3
+            </Nav.Link>
+            <Nav.Link
+              id="navigation"
+              className="underline.gradient"
+              href="#navigation4"
+            >
+              Navigation4
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default ColorSchemesExample;
