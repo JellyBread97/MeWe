fa8e078f8d016a7208f3b455be05676987286d34
Panna Szűcs <szucspann@gmail.com>
2023-01-28 11:06:15 +0100

    Update Jumbo.jsx

--- mewe/src/components/Jumbo.jsx
+++ mewe/src/components/Jumbo.jsx
@@ -1,10 +1,19 @@
-import {Jumbotron} from 'react-bootstrap'
+import Jumbotron from 'react-bootstrap/Jumbotron'
 
 import React from 'react'
 
 export default function Jumbo() {
   return (
-    <Jumbotron></Jumbotron>
+    <Jumbotron>
+  <h1>Hello, world!</h1>
+  <p>
+    This is a simple hero unit, a simple jumbotron-style component for calling
+    extra attention to featured content or information.
+  </p>
+  <p>
+    <Button bsStyle="primary">Learn more</Button>
+  </p>
+</Jumbotron>
   )
 }
 
